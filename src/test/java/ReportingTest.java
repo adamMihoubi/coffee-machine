a58e345583f68dd89814a312b0a71ddab92ff168
adam.mihoubi <adam1309.am@gmail.com>
2020-09-20 17:57:16 +0200

    remove unsused imports

--- src/test/java/ReportingTest.java
+++ src/test/java/ReportingTest.java
@@ -1,7 +1,5 @@
 import domain.MakingDrinks;
 import domain.Order;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
